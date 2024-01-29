482a4d641e9c0a08be588d0491db2868a48ceb97
Srenethesh Mahendren <95108049+srenethesh@users.noreply.github.com>
2024-01-29 21:03:47 +0530

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -3,7 +3,7 @@ from phonenumbers import carrier
 from phonenumbers import geocoder
 
 #Enter the valid number
-number = "+91-9080835402"
+number = "+91-7904809290"
 check_number = phonenumbers.parse(str(number), "CH")
 
 #print the country of the number
